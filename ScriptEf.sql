474c2acbacd420eb94e3ac459603fdfe9f5ab1c1
Jesus Sánchez <2230945@upt.edu.mx>
2024-02-27 23:43:38 -0600

    Diagrams entidad relacion

---
+++ ScriptEf.sql
@@ -0,0 +1,57 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema ejemplos
+-- -----------------------------------------------------
+-- Ejmeplos de transicion
+
+-- -----------------------------------------------------
+-- Schema ejemplos
+--
+-- Ejmeplos de transicion
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ejemplos` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+-- -----------------------------------------------------
+-- Schema ejemplo_unidad3
+-- -----------------------------------------------------
+USE `ejemplos` ;
+
+-- -----------------------------------------------------
+-- Table `ejemplos`.`LIBRO`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ejemplos`.`LIBRO` (
+  `ISBN` INT NOT NULL,
+  `Titulo` VARCHAR(45) NOT NULL,
+  `subtitulo` VARCHAR(45) NULL,
+  `Editorial` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL,
+  `idioma` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ISBN`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ejemplos`.`AUTOR`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ejemplos`.`AUTOR` (
+  `autor` VARCHAR(30) NOT NULL,
+  `LIBRO_ISBN` INT NOT NULL)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ejemplos`.`EDICION`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ejemplos`.`EDICION` (
+  `numero` INT NOT NULL,
+  `año` VARCHAR(45) NOT NULL,
+  `LIBRO_ISBN` INT NOT NULL)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
